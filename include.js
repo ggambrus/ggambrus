513bdd71a8b35e156c1db56867a4bd2b5ea22107
ggambrus <85891502+ggambrus@users.noreply.github.com>
2024-07-16 14:02:45 +0200

    test_029

--- include.js
+++ include.js
@@ -16,3 +16,35 @@ document.addEventListener("DOMContentLoaded", function() {
     loadHTML('header.html', 'header-placeholder');
     loadHTML('footer.html', 'footer-placeholder');
 });
+
+function handleMenu() {
+			document.addEventListener("DOMContentLoaded", function() {
+			const hamburgerIcon = document.getElementById('hamburger-icon');
+			const mobileMenu    = document.getElementById('mobile-menu');
+
+			if (hamburgerIcon) {
+				console.log("Hamburger icon exists");
+			} else {
+				console.error("Hamburger icon not found");
+			}
+
+			hamburgerIcon.addEventListener('click', function(event) {
+				event.stopPropagation();
+				console.log("Hamburger icon clicked");
+				if (mobileMenu.style.display === 'block') {
+					mobileMenu.style.display = 'none';
+					console.log("Hiding mobile menu");
+				} else {
+					mobileMenu.style.display = 'block';
+					console.log("Showing mobile menu");
+				}
+			});
+
+			window.addEventListener('click', function(event) {
+				if (!event.target.closest('.mobile-nav') && !event.target.closest('#mobile-menu')) {
+					mobileMenu.style.display = 'none';
+					console.log("Click outside, hiding mobile menu");
+				}
+			});
+		});
+	};
